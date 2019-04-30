5bfa55ef7d1dcc856923754eb56d2911f6656e8c
Special Projects <linkstasite.cs5@gmail.com>
2019-04-30 12:21:05 -0700

    corrects syntax

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -3,7 +3,7 @@ import { useActions } from 'react-redux';
 import {addTodoAction } from '../redux';
 import uuid from 'uuid/v4';
 
-export default () => {
+const TodoInput = () => {
     const [todo, setTodo] = useState('');
     const addTodo = useActions((todo) => addTodoAction(todo));
     const onChange = event => { setTodo(event.target.value) };
@@ -25,14 +25,17 @@ export default () => {
     return (
         <form onSubmit={onSubmit}>
             <div className="form-div">
-                <input type="text"
+                <input 
+                    type="text"
                     name="todo"
                     placeholder="Add a todo"
                     value={todo}
                     onChange={onChange}
-                    />
-                    <button type="submit">Add Todo</button>
+                />
+                <button type="submit">Add Todo</button>
             </div>
         </form>
-    )
-}
+    );
+};
+
+export default TodoInput;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,22 +2,25 @@ import React from 'react';
 import { useSelector, useActions } from 'react-redux';
 import { toggleTodoAction, deleteTodoAction } from '../redux';
 
-export default () => {
+const TodoList = () => {
     const todos = useSelector((state) => state.todos);
-    const toggleTodo = useActions((todoId) => toggleTodoAction(todoId))
+    const toggleTodo = useActions((todoId) => toggleTodoAction(todoId));
     const deleteTodo = useActions((todoId) => deleteTodoAction(todoId));
     return (
         <ul className="todo-list">
             {todos.map(todo => (
                 <li key={todo.id}>
-                <input type="checkbox"
-                    checked={todo.complete}
-                    onChange={toggleTodo.bind(null, todo.id)}
+                    <input 
+                        type="checkbox"
+                        checked={todo.complete}
+                        onChange={toggleTodo.bind(null, todo.id)}
                     />
                     <span className={todo.complete ? 'complete' : null}>{todo.name}</span>
                     <span className="delete-button" onClick={deleteTodo.bind(null, todo.id)}>X</span>
-                    </li>
-            ))}
+                </li>
+            ))};
         </ul>
     );
 };
+
+export default TodoList;
